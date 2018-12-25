b5d083f4bce378a6b8e1623b933e160af149eb17
Александр <roapwnz2@gmail.com>
2018-12-25 18:11:49 +0300

    changed assert to wait condition

--- gradle/src/test/java/FirstTest.java
+++ gradle/src/test/java/FirstTest.java
@@ -1,28 +1,31 @@
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class FirstTest {
 
     private WebDriver driver;
+    private WebDriverWait wait;
 
     @Before
-    public void setDriver(){
-        //ChromeOptions options = new ChromeOptions();
+    public void start(){
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
     }
 
     @Test
-    public void openGoogle(){
+    public void first(){
         driver.get("https://www.google.com/");
-        Assert.assertEquals(driver.getTitle(), "Google");
+        wait.until(titleIs("Google"));
     }
 
     @After
-    public void tearDown(){
-        if (driver != null) driver.quit();
+    public void stop(){
+        if (driver != null) {driver.quit(); driver = null;}
     }
 }
